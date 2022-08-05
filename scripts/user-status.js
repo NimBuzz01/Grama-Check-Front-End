a9082269f4fb84f2b4e67127547961b23eb06e3e
AbdulHakkam <abdulhakkamazeez@gmail.com>
2022-08-05 16:45:42 +0530

    auth

--- scripts/user-status.js
+++ scripts/user-status.js
@@ -13,7 +13,7 @@ function statusCheck(){
         id_number:id
     },{
         Headers:{
-            'Authorization': `Bearer ${token}`
+            Authorization: `Bearer ${token}`
         }
     }).then(response =>{
         if(response=="invalid"){
