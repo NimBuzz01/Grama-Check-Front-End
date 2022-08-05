70620a9158736e1d01596a4808ef98fe8dcbaadf
AbdulHakkam <abdulhakkamazeez@gmail.com>
2022-08-05 15:31:26 +0530

    auth

--- scripts/pending-requests.js
+++ scripts/pending-requests.js
@@ -7,7 +7,7 @@ function getRequests() {
             grama_id:idNumber
             }, {
                 headers: {
-                    'Authorization': token
+                    'Authorization': 'Bearer '+token
                 }
 }).then(response =>{
     const arr = response.data;
